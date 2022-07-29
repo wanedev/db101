330dabc544ab4205c855707f5f1fc590c1d1285b
wanedev <wanesmith.dev@yahoo.com>
2022-07-28 19:08:37 -0700

    file changes

--- db.js
+++ db.js
@@ -6,6 +6,6 @@ dotenv.config()
 MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true }, function (err, client) {
   module.exports = client
   const app = require("./app.js")
-  app.listen(process.env.PORT || 3000)
+  app.listen(process.env.port || 3000)
   console.log('db connected')
 })
