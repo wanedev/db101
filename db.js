13cb347b018738feccf61fefba65667e53729ffc
wanedev <wanesmith.dev@yahoo.com>
2022-07-30 17:24:46 -0700

    file changes

--- db.js
+++ db.js
@@ -7,5 +7,6 @@ MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true }, functio
   module.exports = client
   const app = require("./app.js")
   app.listen(process.env.port || 3000)
-  console.log('db connected')
+  console.log('db connected' + 'Current server api version:' + JSON.stringify(ServerApiVersion))
+  console.log(ServerApiVersion)
 })
