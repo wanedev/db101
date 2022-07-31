cc8e18bce26c1c8112fc1ae4d3f4620866ce094a
wanedev <wanesmith.dev@yahoo.com>
2022-07-30 17:28:05 -0700

    file changes

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({extended:false}));
 //Find all query
 app.get("/", async (req, res) => {
   try {
+    //Await a connection to mongodb atlas
     const db = await client.db('db101')
     const dbLessons = await db.collection("lessons").find().toArray()
     console.log(JSON.stringify(dbLessons))
 
+//check for lessons inside the database
     if (dbLessons.length) {
       res.json("You have " + dbLessons.length + " document in your collections."+ JSON.stringify(dbLessons))
       console.log("You have " + dbLessons.length + " document in your collections.")
