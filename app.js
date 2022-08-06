7535342700dc361e57a1c1c5de8c136f2ae813a8
wanedev <wanesmith.dev@yahoo.com>
2022-08-05 17:21:50 -0700

    file changes

--- app.js
+++ app.js
@@ -57,13 +57,13 @@ Insert a document into a collection.
 app.get("/database/create", async (req, res) => {
   
   const db = client.db('db101')
-  const dbLessons = await db.collection("lessons").insertOne({title:"Shape drawing",
-    subject:"Drawing",
-    process:"Drawing basic shapes.",
+  const dbLessons = await db.collection("lessons").insertOne({title:"Web programming_101",
+    subject:"web-development",
+    process:"Building web applications.",
     developer:"Lucas Deez"})
 
 //Add a conditional later
-    res.send('You have inserted the following:' + JSON.stringify(dbLessons) + 'Go to the endpoint /database to access the post body')
+    res.send('You have inserted the following:' + JSON.stringify(dbLessons) + 'Go to the endpoint /database to access your newly inserted data.')
   console.log('You have inserted the following:' + JSON.stringify(dbLessons.length))
 
 })
@@ -73,15 +73,32 @@ app.get("/database/create", async (req, res) => {
 //Use different id numbers to check your queries.
 /*In this use case we are looking for one specific document by ObjectId. We can perform
 an update and delete action for that specific document. */
-app.get("/database/find", async (req, res) => {
+app.get("/database/findid", async (req, res) => {
   try {
     const db = client.db('db101')
-    const dbLessons = await db.collection("lessons").find({_id:ObjectId("62e5c5bd98e35a702fa185aa")}).toArray()
+    const dbLessons = await db.collection("lessons").find({_id:ObjectId("62edb2df817cac7d52e60aaf")}).toArray()
     console.log(JSON.stringify(dbLessons))
 
     if (dbLessons) {
       res.json("Object id:"+ JSON.stringify(dbLessons))
       console.log("Object id:"+ JSON.stringify(dbLessons))
+    } 
+  } catch (err) {
+    console.log(err)
+    res.json("Try again later. Not sure where the error occurred.")
+  }
+})
+
+//Update Object or user id
+app.get("/database/updateid", async (req, res) => {
+  try {
+    const db = client.db('db101')
+    const dbLessons = await db.collection("lessons").updateOne({_id:ObjectId("62edb2df817cac7d52e60aaf")},{$set:{developer:"nelson marlow"}})
+    console.log(JSON.stringify(dbLessons))
+
+    if (dbLessons) {
+      res.json("Updated"+ JSON.stringify(dbLessons)+ "go the /database endpoint to see your updated document")
+      console.log("Updated "+ JSON.stringify(dbLessons))
     } 
   } catch (err) {
     console.log(err)
@@ -96,16 +113,13 @@ db.collection.deleteOne()
 Delete the previous insertOne document
 */
 
-app.get("/database/delete", async (req, res) => {
+app.get("/database/deleteid", async (req, res) => {
   
     const db = client.db('db101')
-    const dbLessons = await db.collection("lessons").deleteOne({title:"Shape drawing",
-      subject:"Drawing",
-      process:"Drawing basic shapes.",
-      developer:"Lucas Deez"})
+    const dbLessons = await db.collection("lessons").deleteOne({_id:ObjectId("62edb2df817cac7d52e60aaf")})
 
   //Add a conditional later
-      res.send('You have deleted the following:' + JSON.stringify(dbLessons) + 'Go to the endpoint /database to view the newly removed document.')
+      res.send('You have deleted the following:' + JSON.stringify(dbLessons) + 'Go to the endpoint /database to view your current document.')
     console.log('You have inserted the following:' + JSON.stringify(dbLessons.length))
 
 })
